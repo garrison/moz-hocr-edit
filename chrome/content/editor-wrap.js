92258241992b76f6c7e824a923057ef849928cf7
Jim Garrison <jim@garrison.cc>
2009-03-13 15:31:11 -0400

    side-by-side-layout preference

--- chrome/content/editor-wrap.js
+++ chrome/content/editor-wrap.js
@@ -1,4 +1,9 @@
+const pref_manager = Components.classes["@mozilla.org/preferences-service;1"].getService(Components.interfaces.nsIPrefBranch);
+
 function Startup() {
+  if (pref_manager.getBoolPref("extensions.hocr-edit.side_by_side_layout"))
+    document.firstChild.orient = "horizontal";
+
   var preview = document.getElementById("preview");
   var editor = document.getElementById("editor")
 
--- defaults/preferences/prefs.js
+++ defaults/preferences/prefs.js
@@ -1,1 +1,2 @@
 pref("extensions.hocr-edit.disable_tagsoup_output_filter", false);
+pref("extensions.hocr-edit.side_by_side_layout", false);
