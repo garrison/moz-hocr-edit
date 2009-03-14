b8f46bec158d3551f14c7bd7409e816a08fd3a41
Jim Garrison <jim@garrison.cc>
2009-03-14 02:46:09 -0400

    apply same highlighting rules to the same_word checkbox

--- chrome/content/editor.js
+++ chrome/content/editor.js
@@ -105,10 +105,6 @@ function create_change_func(line, input_element, same_word_element, whitespace_s
   };
 }
 
-function create_onfocus_func(line) {
-  return function () { unhighlight(); highlight(line); };
-}
-
 function load_interface() {
   // figure out page and set image
   var pages = evaluateXPath(preview, "//*[@class='ocr_page']");
@@ -134,12 +130,14 @@ function load_interface() {
     new_input.val(strip(line.innerHTML));
     var change_func = create_change_func(line, new_input, new_same_word, whitespace_suffix)
     new_same_word.change(change_func);
+    new_same_word[0].onblur = unhighlight;
     new_input[0].onkeyup = change_func;
     new_input[0].onkeypress = change_func;
     new_input[0].ondrop = change_func;
     new_input[0].onchange = function () { change_func(); unhighlight(); };
     new_input[0].onblur = unhighlight;
-    new_input[0].onfocus = create_onfocus_func(line);
+    function create_onfocus_func(line) { return function () { highlight(line); }; }
+    new_input[0].onfocus = new_same_word[0].onfocus = create_onfocus_func(line);
     var new_img_span = $(cropped_image_span).clone();
     new_img_span.width(bbox[2] - bbox[0]);
     new_img_span.height(bbox[3] - bbox[1]);
