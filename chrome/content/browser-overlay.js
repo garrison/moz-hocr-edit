3a5a19ed248142b01dddeb0644011fb67c5f7fce
Jim Garrison <jim@garrison.cc>
2009-03-21 00:10:42 -0400

    statusbar overlay menu: always open hocr editor in a new tab

--- chrome/content/browser-overlay.js
+++ chrome/content/browser-overlay.js
@@ -2,3 +2,9 @@ function show_popup(clicked_element, event) {
   if (event.button == 0)
     document.getElementById("hocr-edit-overlay-menu").showPopup(clicked_element, -1, -1, "popup", "topright", "bottomright");
 }
+
+function open_hocr_editor_with_current_document() {
+  var browser = top.document.getElementById("content");
+  var url = 'hocr-edit:' + window.content.document.baseURI;
+  browser.loadOneTab(url, null, null, null, false, false);
+}
