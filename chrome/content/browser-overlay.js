1200b45377737fc6afda4d762ac06299ae7dc6d9
Jim Garrison <jim@garrison.cc>
2009-03-21 21:37:24 -0400

    disable "Edit this hOCR document" menuitem as appropriate
    based in part on linkification.js, which  is a great sample
    implementation of similar functionality

--- chrome/content/browser-overlay.js
+++ chrome/content/browser-overlay.js
@@ -1,4 +1,4 @@
-function show_popup(clicked_element, event) {
+function show_hocr_popup(clicked_element, event) {
   if (event.button == 0)
     document.getElementById("hocr-edit-overlay-menu").showPopup(clicked_element, -1, -1, "popup", "topright", "bottomright");
 }
@@ -8,3 +8,23 @@ function open_hocr_editor_with_current_document() {
   var url = 'hocr-edit:' + window.content.document.baseURI;
   browser.loadOneTab(url, null, null, null, false, false);
 }
+
+function is_hocr_document(doc) {
+  // determine if there's an element with class "ocr_page"
+  var xpe = new XPathEvaluator();
+  var resolver = xpe.createNSResolver(doc);
+  var result = xpe.evaluate("//*[contains(@class,'ocr_page')]", doc, resolver, 0, null);
+  return !!result.iterateNext();
+}
+
+function hocr_onload() {
+  function browser_onfocus() {
+    var doc = getBrowser().contentWindow.document;
+    var edit_current_document = document.getElementById("hocr-edit-open-current-document");
+    edit_current_document.disabled = !is_hocr_document(doc);
+  }
+
+  getBrowser().addEventListener('focus', browser_onfocus, true);
+}
+
+window.addEventListener('load', hocr_onload, false);
