d349187baa5b2f83bad94873b0e01c762d89e9cc
Jim Garrison <jim@garrison.cc>
2009-04-09 10:12:51 -0400

    moved everything in browser_overlay to the hocr_edit namespace
    This shouldn't really be necessary, since all function names already
    contained 'hocr' somewhere, but now we pollute the global namespace less,
    and this change may make it more likely for the extension to pass the
    review process on AMO
    
    see http://blog.mozilla.com/addons/2009/01/16/firefox-extensions-global-namespace-pollution/

--- chrome/content/browser-overlay.js
+++ chrome/content/browser-overlay.js
@@ -1,40 +1,48 @@
-function show_hocr_popup(clicked_element, event) {
-  if (event.button == 0)
-    document.getElementById("hocr-edit-overlay-menu").showPopup(clicked_element, -1, -1, "popup", "topright", "bottomright");
-}
+var hocr_edit = function () {
 
-function open_hocr_editor_with_current_document() {
-  var browser = top.document.getElementById("content");
-  var url = 'hocr-edit:' + window.content.document.baseURI;
-  browser.loadOneTab(url, null, null, null, false, false);
-}
-
-function open_hocr_editor_with_current_image() {
-  open_hocr_editor_with_current_document();
-}
-
-function is_hocr_document(doc) {
-  // determine if there's an element with class "ocr_page"
-  var xpe = new XPathEvaluator();
-  var resolver = xpe.createNSResolver(doc);
-  var result = xpe.evaluate("//*[contains(@class,'ocr_page')]", doc, resolver, 0, null);
-  return !!result.iterateNext();
-}
-
-function is_hocr_image(doc) {
-  return (doc.contentType.substring(0, 6) == "image/");
-}
-
-function hocr_onload() {
-  function browser_onfocus() {
-    var doc = getBrowser().contentWindow.document;
-    var edit_current_document = document.getElementById("hocr-edit-open-current-document");
-    edit_current_document.disabled = !is_hocr_document(doc);
-    var edit_current_image = document.getElementById("hocr-edit-open-current-image");
-    edit_current_image.disabled = !is_hocr_image(doc);
+  function is_hocr_document(doc) {
+    // determine if there's an element with class "ocr_page"
+    var xpe = new XPathEvaluator();
+    var resolver = xpe.createNSResolver(doc);
+    var result = xpe.evaluate("//*[contains(@class,'ocr_page')]", doc, resolver, 0, null);
+    return !!result.iterateNext();
   }
 
-  getBrowser().addEventListener('focus', browser_onfocus, true);
-}
+  function is_hocr_image(doc) {
+    return (doc.contentType.substring(0, 6) == "image/");
+  }
 
-window.addEventListener('load', hocr_onload, false);
+  return {
+
+    show_popup: function (clicked_element, event) {
+      if (event.button == 0)
+	document.getElementById("hocr-edit-overlay-menu").showPopup(clicked_element, -1, -1, "popup", "topright", "bottomright");
+    },
+
+    open_with_current_document: function () {
+      var browser = top.document.getElementById("content");
+      var url = 'hocr-edit:' + window.content.document.baseURI;
+      browser.loadOneTab(url, null, null, null, false, false);
+    },
+
+    open_with_current_image: function () {
+      this.open_with_current_document();
+    },
+
+    onload: function() {
+      function browser_onfocus() {
+	var doc = getBrowser().contentWindow.document;
+	var edit_current_document = document.getElementById("hocr-edit-open-current-document");
+	edit_current_document.disabled = !is_hocr_document(doc);
+	var edit_current_image = document.getElementById("hocr-edit-open-current-image");
+	edit_current_image.disabled = !is_hocr_image(doc);
+      }
+
+      getBrowser().addEventListener('focus', browser_onfocus, true);
+    }
+
+  };
+
+}();
+
+window.addEventListener('load', hocr_edit.onload, false);
